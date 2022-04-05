962ab8eeaea5385816e087cd43757aa821cafa9f
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-04-05 17:00:26 +0200

    Updated function names, variables, improved functions

--- get_weather.py
+++ get_weather.py
@@ -1,17 +1,19 @@
 from decimal import *
 from datetime import date, timedelta, datetime
-import formatting_Utils
-import get_weather_Utils
+import formatting_utils
+import get_weather_utils
 
-# Gets today's date and makes it a string
+
 today = date.today()
-today_str = formatting_Utils.maketimestring(today)
+today_str = formatting_utils.maketimestring(today)
 
-#Gets the city Name and ID from Database (here 0), will be called in a while/for to get all cities
-city = get_weather_Utils.get_location(0)[0]
-city_id = get_weather_Utils.get_location(0)[1]
+cities=[]
+cities = get_weather_utils.get_all_destinations_from_dynamodb()
 
-# makes URL for calling the API for weather in City for the day that is given
-api_url = get_weather_Utils.make_url_api(city_id, today_str)
-#writes weather data to dynamoDB
-get_weather_Utils.write_weather_dynamoDB(api_url, city)
+counter = 0
+for items in cities:
+    
+    api_url = get_weather_utils.determine_url_for_api(cities[counter]['city_id'], today_str)
+
+    get_weather_utils.write_weather_dynamoDB(api_url, cities[counter]['city'])
+    counter+=1
--- get_weather_Utils.py
+++ get_weather_Utils.py
@@ -2,29 +2,33 @@ from decimal import *
 import boto3
 import requests
 from datetime import date, timedelta, datetime
-import formatting_Utils
+import formatting_utils
 
 
 
-def make_url_api(location_id, day_str):
+def determine_url_for_api(location_id, day_str):
     url = "https://www.metaweather.com/api/location/" + location_id + "/"+ day_str  + "/"
-    print("API-URL aus Funktion make_url_api: ", url)
     return url
 
 
-def get_location(number):
+def get_all_destinations_from_dynamodb():
     dynamodb = boto3.resource('dynamodb')
     table_destination = dynamodb.Table('destination')
     response = table_destination.scan()
     data = response['Items']
+    citylist=[]
     while 'LastEvaluatedKey' in response:
         response = table_destination.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])
-    city = response['Items'][number]['city']
-    city_id = response['Items'][number]['city_id']
-    print(city, city_id)
-    return [city, city_id]
-
+        data.extend(response['Items']) 
+    counter = 0
+    for items in data:
+        cities = {
+            "city" : data[counter]['city'],
+            "city_id" : data[counter]['city_id']
+        }
+        citylist.append(cities)
+        counter+=1
+    return citylist
 
 
 def write_weather_dynamoDB(api_url, location):
@@ -38,47 +42,28 @@ def write_weather_dynamoDB(api_url, location):
 
     counter = 0
     for item in response:
-        
-        #city in DB
-        # date = applicable_date
-        report = counter
-        id = response[counter]['id']
-        weather_state_name = response[counter]['weather_state_name']
-        weather_state_abbr = response[counter]['weather_state_abbr']
-        wind_direction_compass = response[counter]['wind_direction_compass']
-        created = response[counter]['created']
-        applicable_date = response[counter]['applicable_date']
-        min_temp = formatting_Utils.make_decimal(response[counter]['min_temp'])
-        max_temp = formatting_Utils.make_decimal(response[counter]['max_temp'])
-        the_temp = formatting_Utils.make_decimal(response[counter]['the_temp'])
-        wind_speed = formatting_Utils.make_decimal(response[counter]['wind_speed'])
-        wind_direction = formatting_Utils.make_decimal(response[counter]['wind_direction'])
-        air_pressure = formatting_Utils.make_decimal(response[counter]['air_pressure'])
-        humidity = response[counter]['humidity']
-        visibility = formatting_Utils.make_decimal(response[counter]['visibility'])
-        predictability = response[counter]['predictability']
-        
-        if formatting_Utils.is_date_recent(created):
+        if not formatting_utils.is_date_recent(response[counter]['created']):
+            continue
+        else:
             dict = {
                 'city'  : location,
                 'date'  : counter,
-                'id'    : id,
-                'weather_state_name' : weather_state_name,
-                'weather_state_abbr' : weather_state_abbr,
-                'wind_direction_compass' : wind_direction_compass,
-                'created'	: created,
-                'applicable_date'	: applicable_date,
-                'min_temp' : min_temp,
-                'max_temp' : max_temp,
-                'the_temp' : the_temp,
-                'wind_speed' : wind_speed,
-                'wind_direction' : wind_direction,
-                'air_pressure' : air_pressure,
-                'humidity' : humidity,
-                'visibility' : visibility,
-                'predictability' : predictability
+                'id'    : response[counter]['id'],
+                'weather_state_name' : response[counter]['weather_state_name'],
+                'weather_state_abbr' : response[counter]['weather_state_abbr'],
+                'wind_direction_compass' : response[counter]['wind_direction_compass'],
+                'created'	: response[counter]['created'],
+                'applicable_date'	: response[counter]['applicable_date'],
+                'min_temp' : formatting_utils.make_decimal(response[counter]['min_temp']),
+                'max_temp' : formatting_utils.make_decimal(response[counter]['max_temp']),
+                'the_temp' : formatting_utils.make_decimal(response[counter]['the_temp']),
+                'wind_speed' : formatting_utils.make_decimal(response[counter]['wind_speed']),
+                'wind_direction' : formatting_utils.make_decimal(response[counter]['wind_direction']),
+                'air_pressure' : formatting_utils.make_decimal(response[counter]['air_pressure']),
+                'humidity' : response[counter]['humidity'],
+                'visibility' : formatting_utils.make_decimal(response[counter]['visibility']),
+                'predictability' : response[counter]['predictability']
             }
-            print("Writing:", created)
             table_weather.put_item(Item=dict)
         counter+=1
 
