c752de4eb6f5a64bbb627d69611d562c9d7c6752
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-04-13 16:59:34 +0200

    Init write cities to dynamo for setting up sunnydays

---
+++ create_sunnydays/write_cities_to_dynamodb.py
@@ -0,0 +1,22 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+
+table_destination = dynamodb.Table('destination')
+
+cities = [
+    {'city': 'London', 'city_id': '44418', 'airport' : 'LOND'}, 
+    {'city': 'Athen', 'city_id': '946738', 'airport' : 'ATH'}, 
+    {'city': 'Lissabon', 'city_id': '742676', 'airport' : 'LIS'},
+    {'city': 'Barcelona', 'city_id': '753692', 'airport' : 'BCN'},
+    {'city': 'Marseille', 'city_id': '610264', 'airport' : 'MRS'}
+    ]
+
+for item in cities:
+    dict = {
+        'city' : item['city'],
+        'city_id' : item['city_id'],
+        'airport' : item['airport']
+    }
+    table_destination.put_item(Item=dict)
