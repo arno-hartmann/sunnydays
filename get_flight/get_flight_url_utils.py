168630c520553d49a1bf997ea3e34c289212c7ee
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-04-22 12:42:46 +0200

    Cleanup: removed comments

--- get_flight/get_flight_url_utils.py
+++ get_flight/get_flight_url_utils.py
@@ -4,9 +4,3 @@ def make_flight_url(origin, destination, departure, arrival):
     url = ("https://www.skyscanner.de/transport/fluge/"+ origin+"/"+ destination+"/"+departure+"/"+arrival+"/")
     print(url)
     return url
-
-
-
-
-
-#make_flight_url("lond", "fra","220421","220424")
