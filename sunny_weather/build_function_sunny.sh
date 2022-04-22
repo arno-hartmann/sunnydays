89c26d90e5e7df74afa279625eaec40df4b9aed0
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-04-22 12:37:54 +0200

    Fixed: removed commentes and cities[]

--- sunny_weather/build_function_sunny.sh
+++ sunny_weather/build_function_sunny.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-#pip3 install --target ./package -r src/requirements.txt
-#pip3 install --target ./package statistics
-#cd package
-#zip -r ../deploy-sunny.zip .
-#cd ..
 cd src
 
 zip -r ../deploy-sunny.zip .
